650e8336551c9cfc9f88b7ef75312bbf2deb4603
ntnhan181 <anitahalley181@gmail.com>
2021-03-30 22:19:34 +0700

    HanCommit

--- src/Shift/Shift.java
+++ src/Shift/Shift.java
@@ -19,7 +19,7 @@ public class Shift {
 	private ArrayList<Staff> ListStaff;
 	private Date WorkingTimeStart;
 	private Date WorkingTimeEnd;
-	
+
 	public Shift(String shiftID, ArrayList<Staff> listStaff, Date workingTimeStart, Date workingTimeEnd) {
 		super();
 		ShiftID = shiftID;
@@ -59,98 +59,122 @@ public class Shift {
 	public void setWorkingTimeStart(Date workingTimeStart) {
 		WorkingTimeStart = workingTimeStart;
 	}
-	
+
 	public static Date readDate(String input) throws ParseException {
 		Scanner sc = new Scanner(System.in);
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
-        return (Date) sdf.parse(input);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy HH:mm:ss");
+		return (Date) sdf.parse(input);
 	}
 
 	public void inputShift() throws ParseException {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter shift ID: ");
 		this.ShiftID = sc.nextLine();
-		
+
 		System.out.println("Enter list of staffs: ");
 		System.out.println("How many staffs?");
 		int n = sc.nextInt();
-		for(int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			this.ListStaff.get(i).inputStaff();
 		}
-		
+
 		System.out.println("Working time starts from: ");
 		String startingTime = sc.nextLine();
 		this.WorkingTimeStart = readDate(startingTime);
 		String endingTime = sc.nextLine();
-		this.WorkingTimeEnd = readDate(endingTime);		
-	}	
-	
+		this.WorkingTimeEnd = readDate(endingTime);
+	}
+
 	public void printShift() {
 		System.out.println("List Shift ");
-		
+
 		System.out.println("Shift ID: " + this.getShiftID());
-		
+
 		System.out.println("List staff: ");
-		for(int i = 0; i < this.ListStaff.size(); i++)
+		for (int i = 0; i < this.ListStaff.size(); i++)
 			this.ListStaff.get(i).printStaff();
-		
+
 		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 		System.out.println("Working time starts from: " + formatter.format(this.getWorkingTimeStart()));
-		System.out.println("Working time ends at: " + formatter.format(this.getWorkingTimeEnd()));		
+		System.out.println("Working time ends at: " + formatter.format(this.getWorkingTimeEnd()));
 	}
-	
+
 	public static boolean checkID(ArrayList<Shift> list, String id) {
-		if(list.size() > 0) {
-			for(Shift st : list) {
-				if(st.getShiftID().compareTo(id) == 0)
+		if (list.size() > 0) {
+			for (Shift st : list) {
+				if (st.getShiftID().compareTo(id) == 0)
 					return true;
 			}
 		}
 		return false;
 	}
-	
+
 	public static void addShiftCmd(ArrayList<Shift> list) throws ParseException, SQLException {
 		String id, startingTime, endingTime;
 		String cmd = "";
-		
+
 		String usernameDB = "han";
 		String passwordDB = "666";
 		String connectionUrl = "jdbc:sqlserver://LAPTOP-IQKFBUD7\\HAN181;databaseName=RestaurantManagement";
-		try(Connection connection = DriverManager.getConnection(connectionUrl, usernameDB, passwordDB);){
-		PreparedStatement ps;
-		
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter shift ID: ");
-		id = sc.nextLine();
-		System.out.println("Working time starts from: ");
-		startingTime = sc.nextLine();
-		Date workingTimeStart = readDate(startingTime);
-		endingTime = sc.nextLine();
-		Date workingTimeEnd = readDate(endingTime);		
-		
-		ArrayList<Staff> l = new ArrayList<>();
-		System.out.println("Enter list of staffs: ");
-		System.out.println("How many staffs?");
-		int n = sc.nextInt();
-		for(int i = 0; i < n; i++) {
-			l.get(i).inputStaff();
-		}
-		
-		if(checkID(list, id) == false) {
-			Shift st = new Shift(id, l, workingTimeStart, workingTimeEnd);
-			for(int i = 0; i < l.size(); i++) {				
-				cmd = "INSERT INTO SinhVien VALUES('" + st.getShiftID() + "','" + st + "','" + st.getListStaff().get(i) + "','" + st.getWorkingTimeStart()+ "','" + st.getWorkingTimeEnd()+"')"; 
-				ps = connection.prepareStatement(cmd);
-				ps.executeUpdate();
+		try (Connection connection = DriverManager.getConnection(connectionUrl, usernameDB, passwordDB);) {
+			PreparedStatement ps;
+
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter shift ID: ");
+			id = sc.nextLine();
+			System.out.println("Working time starts from: ");
+			startingTime = sc.nextLine();
+			Date workingTimeStart = readDate(startingTime);
+			endingTime = sc.nextLine();
+			Date workingTimeEnd = readDate(endingTime);
+
+			ArrayList<Staff> l = new ArrayList<>();
+			System.out.println("Enter list of staffs: ");
+			System.out.println("How many staffs?");
+			int n = sc.nextInt();
+			for (int i = 0; i < n; i++) {
+				l.get(i).inputStaff();
 			}
-				
+
+			if (checkID(list, id) == false) {
+				Shift st = new Shift(id, l, workingTimeStart, workingTimeEnd);
+				for (int i = 0; i < l.size(); i++) {
+					cmd = "INSERT INTO SinhVien VALUES('" + st.getShiftID() + "','" + st + "','"
+							+ st.getListStaff().get(i) + "','" + st.getWorkingTimeStart() + "','"
+							+ st.getWorkingTimeEnd() + "')";
+					ps = connection.prepareStatement(cmd);
+					ps.executeUpdate();
+				}
+
+			} else
+				System.out.println("Try again! ");
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
-		else
-			System.out.println("Try again! ");
-		}catch (SQLException e) {
-			e.printStackTrace();	            
-		}	
 	}
 	
+	public static void removeShiftCmd(ArrayList<Shift> list) {
+		String cmd = "";
+		String usernameDB = "han";
+		String passwordDB = "666";
+		String connectionUrl = "jdbc:sqlserver://LAPTOP-IQKFBUD7\\HAN181;databaseName=RestaurantManagement";
+		try (Connection connection = DriverManager.getConnection(connectionUrl, usernameDB, passwordDB);) {
+			PreparedStatement ps;
+			System.out.println("Enter ID of shift need to remove: ");
+			Scanner sc = new Scanner(System.in);
+			String id = sc.nextLine();
+			if(checkID(list, id)) {
+				cmd = "DELETE FROM SinhVien WHERE ID = '" + id + "'";
+				ps = connection.prepareStatement(cmd);
+	     		ps.executeUpdate();
+	     		System.out.println("Deleted successfully! ");
+			}
+			else
+				System.out.println("This shift does not exist! ");
+		} catch (SQLException e) {
+			e.printStackTrace();	            
+		}
+	}
+
 }
